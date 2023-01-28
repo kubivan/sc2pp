931f96109b1a9c5e213f723159020ab38d47b94f
kubivan <kubivan@users.noreply.github.com>
2023-01-28 03:29:35 +0200

    observation::raw fixed

--- src/Observation.cpp
+++ src/Observation.cpp
@@ -18,6 +18,7 @@ void Observation::update(const proto::ResponseObservation& response_obs)
     const auto [units, unit_commands, errors] = convert_observation(response_obs);
 
     std::tie(m_created, m_damaged, m_self) = m_units.update(units);
+    m_raw = response_obs;
 }
 
 const proto::Observation& Observation::raw() const
